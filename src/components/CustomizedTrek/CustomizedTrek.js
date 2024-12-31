878f983e3a9801fa6217badcdcbf9408ec80f4c1
Himalayan Trails <himalayantrails01@gmail.com>
2024-12-31 19:06:28 +0530

    updated

--- src/components/CustomizedTrek/CustomizedTrek.js
+++ src/components/CustomizedTrek/CustomizedTrek.js
@@ -4,15 +4,15 @@ import CustomizeTrekImage from '../../assets/customize_trek.jpg';
 const CustomizedTrek = ({ trek }) => {
     return (
         <>
-            <div className="margin-bottom-1">
+            <div className="margin-top-20">
                 <div className="section-content">
                     <span className="section-title">Customize Treks</span>
                 </div>
             </div>
             <div className="flex">
-            <img src={require('../../assets/vision.png')} alt="" width="120px" height="80px" />
-            
-            <p style={{marginLeft: "10px"}}>Your vision, our expertise. Let's craft a trekking adventure that's truly unforgettable!</p>
+                <img src={require('../../assets/vision.png')} alt="" width="120px" height="80px" />
+
+                <p style={{ marginLeft: "10px" }}>Your vision, our expertise. Let's craft a trekking adventure that's truly unforgettable!</p>
             </div>
             <div className="trek-card">
                 <img src={CustomizeTrekImage} alt="" width="450px" height="600px" />
--- src/components/WhyChooseUs/WhyChooseUs.js
+++ src/components/WhyChooseUs/WhyChooseUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WhyChooseUs = () => {
     return (
         <>
-            <div className="margin-bottom-1">
+            <div className="margin-top-20">
                 <div className="section-content">
                     <span className="section-title">Why Choose Us</span>
                 </div>
